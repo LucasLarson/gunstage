1d4696220e857166bd50d274e4943e9bb9874dc4
Lucas Larson <LucasLarson@riseup.net>
2020-10-30 19:07:28 -0400

    use `gunstage` as the script’s variable where possible

--- gunstage.plugin.zsh
+++ gunstage.plugin.zsh
@@ -23,15 +23,15 @@ gunstage () {( # 🔫 `git unstage` as a service
   if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
 
     # create a string that we’ll extend and execute below
-    unstage="unset unstage"
+    gunstage="unset gunstage"
 
     # check that this isn’t a new repository
     if git rev-parse HEAD > /dev/null 2>&1; then
-      unstage="$unstage; git restore --staged --progress"
+      gunstage="$gunstage; git restore --staged --progress"
 
     else
       # if the repo is new, it requires different unstaging syntax
-      unstage="$unstage; git rm --cached"
+      gunstage="$gunstage; git rm --cached"
     fi
 
     # check if `gunstage` was called with any arguments, which are
@@ -42,19 +42,19 @@ gunstage () {( # 🔫 `git unstage` as a service
       # run the string as a command
       # https://unix.stackexchange.com/q/356534#comment631887_356534
       # https://github.com/koalaman/shellcheck/wiki/SC2145/c11a85d1d22030fbf846eee09a2dce1f896fa5ae#rationale
-      eval "$unstage $*"
+      eval "$gunstage $*"
 
     else
       # if there were no arguments, we unstage everything in
       # this directory and below
-      eval "$unstage" .
+      eval "$gunstage" .
     fi
 
   else
     # we’re not in a Git repository
     # store the failed if as a return status
     # https://github.com/ohmyzsh/ohmyzsh/pull/9238#discussion_r484806772
-    unstage=$?
+    gunstage=$?
 
     # if `gunstage` is called from outside a Git repository, provide
     # instructions to use it
@@ -68,6 +68,6 @@ gunstage () {( # 🔫 `git unstage` as a service
             gunstage file # huzzah.\n\n
             gunstage # \xf0\x9f\x94\xab \x60git unstage\x60 as a service
             https://github.com/LucasLarson/gunstage\n\n'
-    return "$unstage"
+    return "$gunstage"
   fi
 )}
